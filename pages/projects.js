07d7cfc28ea2f3177872e1e3e6c15189210a0286
Andrew <bigspeedfpv@gmail.com>
2020-09-22 22:38:48 -0400

    begin work on project cards

--- pages/projects.js
+++ pages/projects.js
@@ -12,6 +12,17 @@ export default function Projects() {
       <div className={styles.main}>
         <h1>My Projects</h1>
       </div>
+
+      <Card title="BattPack">Track your batteries with style.</Card>
     </Layout>
   );
+}
+
+function Card({ title, children }) {
+  return (
+    <div className={styles.card}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles.description}>{children}</p>
+    </div>
+  )
 }
