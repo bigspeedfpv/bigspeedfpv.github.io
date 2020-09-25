1e212303f8036676e3098ded7921b66b0c0e8355
Andrew <bigspeedfpv@gmail.com>
2020-09-24 22:46:53 -0400

    finish battpack card

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Projects.module.css";
 import Layout from "../components/layout";
+import Link from "next/link"
 
 export default function Projects() {
   return (
@@ -13,7 +14,15 @@ export default function Projects() {
         <h1>My Projects</h1>
       </div>
 
-      <Card title="BattPack">Track your batteries with style with BattPack. BattPack features charge cycle tracking, discharge reminder notificaitons, and a notes section for each battery, so you never lose track of your rechargable batteries again.</Card>
+      <Card
+        title="BattPack"
+        link="https://apps.apple.com/us/app/battpack/id1522165410"
+      >
+        Track your batteries with style with BattPack. BattPack features charge
+        cycle tracking, discharge reminder notificaitons, and a notes section
+        for each battery, so you never lose track of your rechargable batteries
+        again.
+      </Card>
     </Layout>
   );
 }
@@ -21,8 +30,12 @@ export default function Projects() {
 function Card({ title, children }) {
   return (
     <div className={styles.card}>
-      <h1 className={styles.title}>{title}</h1>
-      <p className={styles.description}>{children}</p>
+      <Link href="https://apps.apple.com/us/app/battpack/id1522165410">
+        <a>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.description}>{children}</p>
+        </a>
+      </Link>
     </div>
-  )
+  );
 }
