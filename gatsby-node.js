434e6f1bdc1f5979b8e2cb5c906fc57442888ee1
Andrew <bigspeedfpv@gmail.com>
2020-12-16 12:39:57 -0500

    🚑 Fix gatsby-node.js not awaiting axios response

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,26 +1,22 @@
 const axios = require("axios")
 
-exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
   const { createNode } = actions
-
-  var b64 = ""
   
-  axios
+  const b64 = await axios
     .get("https://github.com/bigspeedfpv.png", {
       responseType: 'arraybuffer'
     })
-    .then(response => {
-      b64 = Buffer.from(response.data, 'binary').toString('base64')
+    .then(response => Buffer.from(response.data, 'binary').toString('base64'))
 
-      const node = {
-        name: "Profile",
-        id: createNodeId(`Profile`),
-        data: b64,
-        internal: {
-          type: "Profile",
-          contentDigest: createContentDigest(b64)
-        }
-      }
-      actions.createNode(node)
-    })
+  const node = {
+    name: "Profile",
+    id: createNodeId(`Profile`),
+    data: b64,
+    internal: {
+      type: "Profile",
+      contentDigest: createContentDigest(b64)
+    }
+  }
+  actions.createNode(node)
 }
