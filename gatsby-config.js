580ed951242b2998250b1d0d10cf61ec0ae6ec32
Andrew <bigspeedfpv@gmail.com>
2021-02-21 10:27:00 -0500

    🔥 Remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,9 +18,7 @@ module.exports = {
         icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-styled-components`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-styled-components`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
