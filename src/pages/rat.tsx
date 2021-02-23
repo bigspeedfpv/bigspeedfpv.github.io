dcd2c66dafe572d3860fc6bc85d62e72d844aa6b
Andrew <bigspeedfpv@gmail.com>
2021-02-22 19:27:31 -0500

    ✨ rat-ify

---
+++ src/pages/rat.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Background, Container, RateliusTitle } from "../styles/rat"
+
+const Rat = () => {
+  const [loaded, setLoaded] = React.useState(false)
+
+  return (
+    <>
+      <Container>
+        <Background src="rat.gif" onLoad={() => setLoaded(true)} visible={loaded} />
+      </Container>
+      <Container>
+        <RateliusTitle>my boy ratelius is an absolute unit so he takes a sec to load</RateliusTitle>
+      </Container>
+    </>
+  )
+}
+
+export default Rat
