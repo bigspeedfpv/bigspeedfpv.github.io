11509bdbc1c61590028df406171a9fd522c7af98
Andrew <bigspeedfpv@gmail.com>
2021-02-22 20:18:33 -0500

    🐛 Load rat even when cached

--- src/pages/rat.tsx
+++ src/pages/rat.tsx
@@ -4,10 +4,16 @@ import { Background, Container, RateliusTitle } from "../styles/rat"
 const Rat = () => {
   const [loaded, setLoaded] = React.useState(false)
 
+  const ratGif = React.useRef()
+
+  React.useEffect(() => {
+    if ((ratGif.current as any).loaded) setLoaded(true)
+  })
+  
   return (
     <>
       <Container>
-        <Background src="/rat.gif" onLoad={() => setLoaded(true)} visible={loaded} />
+        <Background src="/rat.gif" onLoad={() => setLoaded(true)} visible={loaded} ref={ratGif} />
       </Container>
       <Container>
         <RateliusTitle>my boy ratelius is an absolute unit so he takes a sec to load</RateliusTitle>
