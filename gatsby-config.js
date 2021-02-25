3cd10f4dfce4744867435c8cb5cdf0fa0bc27796
Andrew <bigspeedfpv@gmail.com>
2021-02-25 16:02:25 -0500

    🍱 Change site meta info

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `bigspeed`,
+        short_name: `bigspeed`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ff0000`,
+        theme_color: `#ff0000`,
         display: `minimal-ui`,
         icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
