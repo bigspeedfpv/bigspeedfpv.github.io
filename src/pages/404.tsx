b747eafa63796fed60eb07b0d16b3aa3b50261f5
Andrew <bigspeedfpv@gmail.com>
2021-02-25 16:01:27 -0500

    ✨ Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import SEO from "../components/seo"
+
+import { Bold404, Container404, Desc404, Link404, Title404 } from "../styles/404"
+
+const NotFoundPage = () => (
+  <>
+    <SEO title="404 Not Found" />
+    <Container404>
+      <Title404><Bold404>Page not found.</Bold404> That's all we know.</Title404>
+      <Desc404>You can try <Link404 to="/">going Home</Link404>.</Desc404>
+    </Container404>
+  </>
+)
+
+export default NotFoundPage
---
+++ src/styles/404.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+export const Container404 = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+
+  width: 100%;
+  height: 100vh;
+
+  background-color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+`
+
+export const Title404 = styled.span`
+  font-size: 50px;
+  margin: 25px;
+  
+  text-align: center;
+`
+
+export const Bold404 = styled.strong`
+  font-weight: 700;
+
+  display: inline-block;
+  line-height: 47px;
+
+  text-shadow: 3px 3px white, 3px -3px white, -3px 3px white, -3px -3px white;
+
+  border-bottom: 4px solid transparent;
+
+  border-image: linear-gradient(90deg, rgba(255,0,155,1) 0%, rgba(255,136,0,1) 100%);
+  
+  border-image-slice: 1;
+
+  &&& {
+    &::selection {
+      text-shadow: none;
+    }
+  }
+`
+
+export const Desc404 = styled.span`
+  font-size: 30px;
+`
+
+export const Link404 = styled(Link)`
+  color: #ff4b33;
+
+
+`
