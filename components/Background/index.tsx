8a5f874bd626814bbad6d524e18517c6733921f4
Andrew <bigspeedfpv@gmail.com>
2021-05-08 23:53:46 -0400

    🎉 Start groundwork for Next.js rewrite

---
+++ components/Background/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { BackgroundContainer, BackgroundImage } from "./style"
+
+const Background = () => {
+  return (
+    <BackgroundContainer>
+      <BackgroundImage />
+    </BackgroundContainer>
+  )
+}
---
+++ components/Background/style.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import styled from "styled-components"
+import Image from "next/image"
+
+export const BackgroundContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const BackgroundImage = styled(Image)`
+  
+`
