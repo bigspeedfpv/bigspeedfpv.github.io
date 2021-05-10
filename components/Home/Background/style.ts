5e82ff4ab59ac67b21911420bf90d948516398e6
Andrew <bigspeedfpv@gmail.com>
2021-05-10 04:35:07 +0000

    🐛 Fix vertical spacing on WebKit

--- components/Home/Background/style.ts
+++ components/Home/Background/style.ts
@@ -3,20 +3,20 @@ import styled from "styled-components"
 
 export const BackgroundContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
 
   display: flex;
   align-items: center;
   justify-content: center;
 
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
 `
 
 export const BackgroundImage = styled.img`
   filter: brightness(75%) blur(6px);
-  transform: scale(1.1);
+  transform: scale(1.2);
 
   min-width: 100vw;
   min-height: 100vh;
--- components/Home/Name/style.ts
+++ components/Home/Name/style.ts
@@ -4,14 +4,14 @@ import Image from "next/image"
 
 export const NameContainer = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
 
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
 `
